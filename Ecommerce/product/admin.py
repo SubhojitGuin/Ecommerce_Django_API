4c7268e9bcd5e3ecc760227d94fcf425be3fbb32
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2024-10-26 20:59:04 +0530

    modified admin.py

--- Ecommerce/product/admin.py
+++ Ecommerce/product/admin.py
@@ -3,6 +3,7 @@ from .models import *
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at']
     list_display = ['id', 'name','description','price','quantity','category','brand','created_at']
     list_filter = ['category','price','brand','quantity', 'created_at']
     search_fields = ['quantity', 'brand', 'category', 'price', 'name']
--- Ecommerce/user/admin.py
+++ Ecommerce/user/admin.py
@@ -3,6 +3,9 @@ from .models import *
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
+
+    readonly_fields = ['email','password','created_at']
+
     list_display = ['id', 'first_name','last_name','email', 'phone', 'address', 'city', 'country', 'pin_code','created_at']
     list_filter = ['created_at']
     search_fields = ['name', 'email', 'phone', 'address']
@@ -11,9 +14,19 @@ class UserAdmin(admin.ModelAdmin):
 admin.site.register(User, UserAdmin)
 
 class WishlistAdmin(admin.ModelAdmin):
-    list_display = ['user','product','created_at']
+    readonly_fields = ['created_at']
+    list_display = ['user','product','created_at','get_user_id','get_product_id']
     list_filter = ['created_at']
     search_fields = ['user', 'product']
     list_per_page = 10
+
+    
+    def get_user_id(self, obj):
+        return obj.user.id
+    get_user_id.short_description = 'user_id'  
+
+    def get_product_id(self, obj):
+        return obj.product.id
+    get_product_id.short_description = 'product_id' 
     
 admin.site.register(Wishlist, WishlistAdmin)
