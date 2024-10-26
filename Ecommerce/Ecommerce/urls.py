40af71b5a011750a92735f4d6df4e9c33732979f
Subhojit Guin <subhojitguin2004@gmail.com>
2024-10-26 15:57:35 +0530

    Add Product App

--- Ecommerce/Ecommerce/urls.py
+++ Ecommerce/Ecommerce/urls.py
@@ -22,6 +22,7 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
+    path('product/', include('product.urls')),
 ]
 
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Ecommerce/product/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('list/', views.product_list, name='product_list'),
+    path('detail/', views.product_detail, name='product_detail'),
+    path('create/', views.product_create, name='product_create'),
+    path('update/', views.product_update, name='product_update'),
+    path('delete/', views.product_delete, name='product_delete'),
+    path('search/', views.product_search, name='product_search'),
+    path('filterby_category/', views.product_category, name='product_category'),
+    path('filterby_brand/', views.product_brand, name='product_brand'),
+    path('filterby_price/', views.product_price, name='product_price')
+]
